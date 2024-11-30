f1c63a2e1ec9635564365370eb830a967f176513
gustavoagui1 <gustavo.muu@hotmail.com>
2024-11-30 17:27:56 -0300

    Modelo BASICO de Recetas
    Se creó un modelo muy basico de recetas. Este modelo sirve solo para probar y SE DEBE MEJORAR PARA EL TRABAJO FINAL.

--- Recetas/views.py
+++ Recetas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from Recetas.models import Receta
 # Create your views here.
+
+def recetasData(request):
+    recetas = Receta.objects.all()
+    data = {'recetas':recetas}
+    return render(request, 'Recetas/recetas.html',data)
