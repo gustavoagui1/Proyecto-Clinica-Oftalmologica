168bacf97ac1f3dad0e77da0af9c855bd2751996
gustavoagui1 <gustavo.muu@hotmail.com>
2024-12-16 19:01:51 -0300

    Se creo modelo de agenda

--- Agenda/models.py
+++ Agenda/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Oftalmologos.models import Oftalmologo
 
 # Create your models here.
+class Agenda(models.Model):
+    fecha = models.DateField()
+    hora = models.TimeField()
+    oftalmologo = models.ForeignKey(Oftalmologo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Fecha: {self.fecha} - Hora: {self.hora} - Oftalmologo: {self.oftalmologo}'
